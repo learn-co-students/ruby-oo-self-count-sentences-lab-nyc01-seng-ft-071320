40e8bf180f3d35830496e3178bc39b1526f84222
humorous-storage-7521 <rmwerner@umich.edu>
2020-07-14 23:04:28 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else false
+    end
   end
 
   def count_sentences
-
+    delimiters = [".", "!", "?"]
+    array = self.split(Regexp.union(delimiters))
+    array.each do |string|
+      if string == ""
+        array.delete(string)
+      end
+    end
+    array.length
   end
 end
